5b9cabd2f4852f064e5b585d8bf33c9650eb9f36
rahul-deolikar <rahuldeolikar@outlook.com>
2024-06-18 17:42:46 +0530

    Update script.sh

--- terraform/script.sh
+++ terraform/script.sh
@@ -1,3 +1,7 @@
+//for use of httpd server installation script
+
+user_data =<<-EOF
+
 #!/bin/bash
 sudo -i
 yum install httpd -y
@@ -5,10 +9,18 @@ systemctl start httpd
 systemctl enable httpd
 echo "hello apache" >/var/www/html/index.html
 
+EOF
 
+
+
+//for use of nginx server installation script
+
+user_data =<<-EOF
 #!/bin/bash
 sudo -i
 yum install nginx -y
 systemctl start nginx.service
 systemctl enable nginx.service
 echo "hello apache" >/usr/share/html/index.html
+
+EOF
