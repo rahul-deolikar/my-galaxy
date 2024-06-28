1fc931039958e26cf49fb9dfeb78220d74cd93bd
rahul-deolikar <rahuldeolikar@outlook.com>
2024-06-28 19:38:11 +0530

    Create variables.tf
    this is parent module variables main file

---
+++ terraform/modules/variables.tf
@@ -0,0 +1,47 @@
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "key_name" {
+    type = string
+}
+
+variable "instance_name" {
+    type = string
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "subnet_cidr_block" {
+    type = string
+}
+
+variable "subnet_az" {
+  type = string
+}
+
+variable "public_ip" { 
+  type = bool
+}
+
+variable "subnet_name" {
+    type = string
+}
+
+variable "igw_name" {
+  type = string
+}
+
+variable "ports" {
+  type = list(number)
+}
