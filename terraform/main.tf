21da1cb1ad417e4a1f39c5c678f3ebe2305b3b97
rahul-deolikar <rahuldeolikar@outlook.com>
2025-03-20 14:05:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,16 @@ provider "aws" {
   profile  = "B-34"
 }
 
+resource "aws_instance" "vm-1" {
+    ami = "ami-04b70fa74e45c3917"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+    key_name = "B-34"
+     tags = {
+        name = "server-1"
+     }
+}
+
 resource "aws_iam_user" "user-1" {
   name = "usr-1"
 }
